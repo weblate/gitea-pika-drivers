ab464df43147a7c29f9461e0e8cb2634a0ef6706
Ward Nakchbandi (Cosmic Fusion) <hotrod.master@hotmail.com>
2023-06-30 02:21:58 +0300

    fixed cusred amdgpu entries

--- data/generate_driver_definitions.sh
+++ data/generate_driver_definitions.sh
@@ -1,22 +1,20 @@
 #! /bin/bash
+
 ### Get traditional ubuntu-driver list
-UBUNTU_DRIVERS="$(ubuntu-drivers list | grep -vi -server)"
+DRIVERS="$(ubuntu-drivers list | grep -vi -server)"
 
 ### Check if the amdgpu module is loaded
 ### advanced micro devices graphics cards with "radeon" module only do not support the additional drivers
-if inxi -G | grep " loaded" | grep "amdgpu" &> /dev/null
+if inxi -G | grep driver | grep amdgpu &> /dev/null
 then
-	AMDGPU_DRIVERS="$(echo -e "pika-rocm-meta\nvulkan-amdgpu-pro\namf-amdgpu-pro\namdvlk\nopencl-legacy-amdgpu-pro-icd\namdgpu-pro-oglp")"
+	DRIVERS="$DRIVERS pika-rocm-meta vulkan-amdgpu-pro amf-amdgpu-pro amdvlk opencl-legacy-amdgpu-pro-icd amdgpu-pro-oglp"
 fi
 
 ### Check for xbox equipment
 if lsusb | grep -i xbox  &> /dev/null
 then
-	XONE_DRIVERS="$(echo -e "xone-dkms")"
+	DRIVERS="$DRIVERS xone-dkms xpadneo"
 fi
-
-### Merge all drivers together
-DRIVERS="$(printf "$UBUNTU_DRIVERS\n$AMDGPU_DRIVERS\n$XONE_DRIVERS")"
 
 ### If no drivers were found set them to random hash to trigger no drivers needed dialog in gui app
 if [[ -z $DRIVERS ]]
@@ -25,4 +23,4 @@ then
 fi
 
 ### Print result
-echo $DRIVERS | tr " " "\n"
+echo -e "$DRIVERS" | tr ' ' '\n' | grep "\S"
