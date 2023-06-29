227961ee6ba9e69ed560ed6ec4a908d6ee7b81a7
Ward Nakchbandi (Cosmic Fusion) <hotrod.master@hotmail.com>
2023-06-30 00:12:57 +0300

    makefile

---
+++ main.sh
@@ -0,0 +1,14 @@
+# Clone Upstream
+mkdir -p pika-drivers
+cp -rvf ./* ./pika-drivers/
+
+# Get build deps
+apt-get build-dep ./ -y
+
+# Build package
+dpkg-buildpackage --no-sign
+
+# Move the debs to output
+cd ../
+mkdir -p ./output
+mv ./*.deb ./output/
