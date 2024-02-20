c6a938f2a02dcf990c37d5fbd867808832f3d371
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-02-21 00:47:27 +0300

    Update dialog-success.sh

--- data/dialog-success.sh
+++ data/dialog-success.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
+WHO=$(whoami)
+if [[ $WHO == "pikaos" ]]; then
+zenity --info --text ""$1" has been processed successfully."
+else
 if echo $1 | grep -i mesa
 then
-    true
+    zenity --info --text ""$1" has been processed successfully."
 else
     if zenity --question --text ""$1" has been processed successfully. Would you like to reboot for changes to take effect?"
     then
 	    systemctl reboot
     fi
+fi
 fi
