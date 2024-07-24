e36800779d1c66bae958e105cbbbf77513c476b3
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-07-24 14:00:56 +0300

    fix main

---
+++ pika-build-config/amd64-v3.sh
@@ -0,0 +1,9 @@
+PIKA_BUILD_ARCH="amd64-v3"
+DEBIAN_FRONTEND="noninteractive"
+DEB_BUILD_MAINT_OPTIONS="optimize=+lto -march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+DEB_CFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+DEB_CPPFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+DEB_CXXFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+DEB_LDFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+DEB_BUILD_OPTIONS="nocheck notest terse"
+DPKG_GENSYMBOLS_CHECK_LEVEL=0
