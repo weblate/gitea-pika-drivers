f84535f74f956fe6ac5d0f531c39fcfb5bf646d9
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-07-24 04:21:44 +0300

    add workflows

--- src/config.rs
+++ src/config.rs
@@ -2,5 +2,4 @@ pub const APP_ID: &str = "com.github.pikaos-linux.pikadrivers";
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const APP_ICON: &str = "com.github.pikaos-linux.pikadrivers";
 pub const APP_GITHUB: &str = "https://github.com/PikaOS-Linux/pkg-pika-drivers";
-pub const DRIVER_DB_JSON_URL: &str =
-    "https://raw.githubusercontent.com/PikaOS-Linux/pkg-pika-drivers/main/driver-db.json";
+pub const DRIVER_DB_JSON_URL: &str = "https://git.pika-os.com/custom-gui-packages/pika-drivers/raw/branch/main/driver-db.json";
