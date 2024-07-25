1768b196b1097b86b46e52212e88e95207bf85f0
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-07-26 01:17:01 +0300

    fix maint env

--- pika-build-config/amd64-v3.sh
+++ pika-build-config/amd64-v3.sh
@@ -1,9 +1,10 @@
-PIKA_BUILD_ARCH="amd64-v3"
-DEBIAN_FRONTEND="noninteractive"
-DEB_BUILD_MAINT_OPTIONS="optimize=+lto -march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
-DEB_CFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
-DEB_CPPFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
-DEB_CXXFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
-DEB_LDFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
-DEB_BUILD_OPTIONS="nocheck notest terse"
-DPKG_GENSYMBOLS_CHECK_LEVEL=0
+#! /bin/bash
+export PIKA_BUILD_ARCH="amd64-v3"
+export DEBIAN_FRONTEND="noninteractive"
+export DEB_BUILD_MAINT_OPTIONS="optimize=+lto -march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_CFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_CPPFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_CXXFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_LDFLAGS_MAINT_APPEND="-march=x86-64-v3 -O3 -flto -fuse-linker-plugin -falign-functions=32"
+export DEB_BUILD_OPTIONS="nocheck notest terse"
+export DPKG_GENSYMBOLS_CHECK_LEVEL=0
---
+++ pika-build-config/i386.sh
@@ -0,0 +1,4 @@
+export PIKA_BUILD_ARCH="i386"
+export DEBIAN_FRONTEND="noninteractive"
+export DEB_BUILD_OPTIONS="nocheck notest terse"
+export DPKG_GENSYMBOLS_CHECK_LEVEL=0
